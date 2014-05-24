bff1428bfbb4d1891f6b026b1c61e74912acae77
颜风 <1178328901@qq.com>
2014-05-24 14:59:53 +0800

    Initial Commit

---
+++ CityManager/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CityManager
+//
+//  Created by   颜风 on 14-5-24.
+//  Copyright (c) 2014年 Shadow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
